9596d80a22c6e5c6998257117db97e7ba4afb8c5
chentsulin <chentsulin@gmail.com>
2020-03-09 17:41:05 +0800

    docs: setup crowdin

---
+++ website/languages.js
@@ -0,0 +1,19 @@
+const languages = [
+  {
+    enabled: true,
+    name: 'English',
+    tag: 'en',
+  },
+  {
+    enabled: true,
+    name: '日本語',
+    tag: 'ja',
+  },
+  {
+    enabled: true,
+    name: '繁體中文',
+    tag: 'zh-TW',
+  },
+];
+
+module.exports = languages;
